1a2d831f1f445c0d3ed310e15532b35825d120c3
miketriano <mtriano@outlook.com>
2017-04-07 13:44:09 -0400

    chess

---
+++ Chess/src/pieces/Piece.java
@@ -0,0 +1,38 @@
+package pieces;
+
+import app.*;
+
+/**
+ * The basis for a standard chess piece
+ *
+ * @author Michael Triano
+ * @author Joseph Wolak
+ */
+public abstract class Piece {
+    /**
+     * Tracks if the piece has made a move yet
+     */
+    public boolean firstMove;
+    /**
+     * Tracks if pawn is eligable for promotion
+     */
+    public char promotion;
+    /**
+     * Tracks if en passant is possible
+     */
+    public char enpassant;
+    /**
+     * Tracks if castling is possible
+     */
+    public char castling;
+
+    /**
+     * Used by each chess piece to validate if a move is legal
+     *
+     * @param start Where the piece is currently
+     * @param end Where the piece wants to go
+     * @param board The chess board
+     * @return True if the piece can move ot the end point legally, false otherwise
+     */
+    public abstract boolean isValidMove(Point start, Point end, Board board);
+}
